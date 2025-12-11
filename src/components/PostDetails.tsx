98686bb5060cb01bc24c128f049e42f21b048f99
Artem Yakhno <artemen23093@gmail.com>
2025-12-11 14:26:00 +0200

    added solution

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,107 +1,145 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from './Loader';
 import { NewCommentForm } from './NewCommentForm';
+import { Post } from '../types/Post';
+import * as apiComments from '../api/comments';
+import { Comment, CommentData } from '../types/Comment';
 
-export const PostDetails: React.FC = () => {
+type Props = {
+  post: Post;
+};
+
+const PostDetailsComponent: React.FC<Props> = ({ post }) => {
+  const [loadingCommets, setLoadingComments] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [postComments, setPostComments] = useState<Comment[]>([]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const addComment = async ({ name, email, body }: CommentData) => {
+    try {
+      const newComment = await apiComments.addComment({
+        name,
+        email,
+        body,
+        postId: post.id,
+      });
+
+      setPostComments(prev => {
+        return [...prev, newComment];
+      });
+    } catch {
+      setErrorMessage('Failed to add comment to server');
+      throw new Error();
+    }
+  };
+
+  const deleteComment = async (commentId: number) => {
+    try {
+      setPostComments(prevComments => {
+        return prevComments.filter(comment => comment.id !== commentId);
+      });
+      await apiComments.deletePostComment(commentId);
+    } catch (error) {
+      setErrorMessage('Failed to delete comments from server');
+      setPostComments(postComments);
+    }
+  };
+
+  useEffect(() => {
+    const getPostCommentsFromServer = async () => {
+      try {
+        setErrorMessage('');
+        setLoadingComments(true);
+        const commentsFromServer = await apiComments.getPostComment(post.id);
+
+        setPostComments(commentsFromServer);
+      } catch {
+        setErrorMessage('Failed to get comments from server');
+      } finally {
+        setLoadingComments(false);
+      }
+    };
+
+    getPostCommentsFromServer();
+  }, [post]);
+
+  const isPlugVisible =
+    postComments.length === 0 && !loadingCommets && !errorMessage;
+  const isCommentsVisible = postComments.length > 0 && !loadingCommets;
+
   return (
     <div className="content" data-cy="PostDetails">
       <div className="content" data-cy="PostDetails">
         <div className="block">
           <h2 data-cy="PostTitle">
-            #18: voluptate et itaque vero tempora molestiae
+            #{post.id}: {post.title}
           </h2>
 
-          <p data-cy="PostBody">
-            eveniet quo quis laborum totam consequatur non dolor ut et est
-            repudiandae est voluptatem vel debitis et magnam
-          </p>
+          <p data-cy="PostBody">{post.body}</p>
         </div>
 
         <div className="block">
-          <Loader />
+          {loadingCommets && <Loader />}
 
-          <div className="notification is-danger" data-cy="CommentsError">
-            Something went wrong
-          </div>
-
-          <p className="title is-4" data-cy="NoCommentsMessage">
-            No comments yet
-          </p>
-
-          <p className="title is-4">Comments:</p>
-
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
+          {errorMessage && (
+            <div className="notification is-danger" data-cy="CommentsError">
+              {errorMessage}
             </div>
+          )}
 
-            <div className="message-body" data-cy="CommentBody">
-              Some comment
-            </div>
-          </article>
+          {isPlugVisible && (
+            <p className="title is-4" data-cy="NoCommentsMessage">
+              No comments yet
+            </p>
+          )}
 
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
+          {isCommentsVisible && (
+            <>
+              <p className="title is-4">Comments:</p>
 
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-            <div className="message-body" data-cy="CommentBody">
-              One more comment
-            </div>
-          </article>
+              {postComments.map(comment => (
+                <article
+                  key={comment.id}
+                  className="message is-small"
+                  data-cy="Comment"
+                >
+                  <div className="message-header">
+                    <a href={`mailto:${comment.email}`} data-cy="CommentAuthor">
+                      {comment.name}
+                    </a>
+                    <button
+                      data-cy="CommentDelete"
+                      type="button"
+                      className="delete is-small"
+                      aria-label="delete"
+                      onClick={() => deleteComment(comment.id)}
+                    >
+                      delete button
+                    </button>
+                  </div>
 
-          <article className="message is-small" data-cy="Comment">
-            <div className="message-header">
-              <a href="mailto:misha@mate.academy" data-cy="CommentAuthor">
-                Misha Hrynko
-              </a>
-
-              <button
-                data-cy="CommentDelete"
-                type="button"
-                className="delete is-small"
-                aria-label="delete"
-              >
-                delete button
-              </button>
-            </div>
-
-            <div className="message-body" data-cy="CommentBody">
-              {'Multi\nline\ncomment'}
-            </div>
-          </article>
-
-          <button
-            data-cy="WriteCommentButton"
-            type="button"
-            className="button is-link"
-          >
-            Write a comment
-          </button>
+                  <div className="message-body" data-cy="CommentBody">
+                    {comment.body}
+                  </div>
+                </article>
+              ))}
+            </>
+          )}
+          {!isFormVisible && (
+            <button
+              data-cy="WriteCommentButton"
+              type="button"
+              className="button is-link"
+              onClick={() => setIsFormVisible(true)}
+            >
+              Write a comment
+            </button>
+          )}
         </div>
-
-        <NewCommentForm />
+        {isFormVisible && <NewCommentForm onSubmit={addComment} />}
       </div>
     </div>
   );
 };
+
+export const PostDetails = React.memo(PostDetailsComponent);
